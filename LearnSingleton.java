019e227de424ccc5ba3351aee687e0a806941504
Abhishek Singhal <abhisheksinghal8086@gmail.com>
2017-05-30 20:53:10 +0530

    Delete LearnSingleton.java

--- LearnSingleton.java
+++
@@ -1,9 +0,0 @@
-class LearnSingleton{
-	
-	public static void main(String args[]){
-	Singleton obj=Singleton.getInstance();
-		System.out.println(obj);
-		Singleton obj1=obj.getInstance();
-		System.out.println(obj1);
-	}
-}
